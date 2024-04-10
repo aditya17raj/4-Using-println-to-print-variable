5fb3da4b57f3b4d375691447af0190bf98b602fb
Aditya <141512093+aditya17raj@users.noreply.github.com>
2024-04-10 14:59:01 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+
+    System.out.println("Printint string");
+    
+    System.out.println("Value of a = "+a);
+    System.out.println("Value of b = "+b);
+
+    System.out.println("value of a + b = " + (a+b)     + "\n Value of a - b = " + (a-b) + "\n Value       of a * b = " + (a*b));
+  }
+
+}
